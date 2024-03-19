7721cdf653dfee0a085ff106977eb8008f4b257c
Geek_Tech <geecktech.och2@gmail.com>
2024-03-19 20:16:23 +0600

    'ls7.1'

--- les7.py
+++ les7.py
@@ -26,6 +26,23 @@ c.execute('''CREATE TABLE IF NOT EXISTS user(
 # CREATE - INSERT INTO
 
 c.execute('''INSERT INTO user VALUES ('бека', 'text',12,'2003-06-06','ogil')''')
+c.execute('''INSERT INTO user (имя,возраст) VALUES ('beka',18)''')
+
+
+# UPDATE - UPDATE
+c.execute('''UPDATE user SET возраст=18 WHERE rowid<3 ''')
+
+
+# delete - delete
+c.execute('''DELETE FROM user WHERE rowid=2''')
+
+
+# REED-SELECT return
+c.execute('''SELECT rowid,* FROM user ORDER BY rowid DESC ''')
+item=c.fetchall()
+for i in item:
+    print(i)
+
 
 
 
